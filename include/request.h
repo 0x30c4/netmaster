8f4c3bfabe110ff4cb0fb15fcba0e43cf1cf4167
0x30c4 <abdullahsanaf@gmail.com>
2020-05-30 19:56:21 +0600

    .

--- include/request.h
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-// #include "server.h"
-
-void requestType(int, int);
-void postHandler(int, const char *, int);
-void headerSender(int, int);
-void getHandler(int, const char *);
